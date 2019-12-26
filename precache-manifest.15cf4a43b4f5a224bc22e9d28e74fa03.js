1047979914f39fd2fed9c0b4c75d2706e6347ac2
buqento <buqento@gmail.com>
2019-12-26 02:24:52 -0800

    Updates

---
+++ precache-manifest.15cf4a43b4f5a224bc22e9d28e74fa03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875229faca5d75b9ec2daaa87bf5dd62",
+    "url": "/dkos/index.html"
+  },
+  {
+    "revision": "981e9e770b7c9b2750a5",
+    "url": "/dkos/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "ed4f355fb177e3892c42",
+    "url": "/dkos/static/css/main.e55ea047.chunk.css"
+  },
+  {
+    "revision": "981e9e770b7c9b2750a5",
+    "url": "/dkos/static/js/2.7cf61c60.chunk.js"
+  },
+  {
+    "revision": "ed4f355fb177e3892c42",
+    "url": "/dkos/static/js/main.e08d5169.chunk.js"
+  },
+  {
+    "revision": "60f55be4fbbc53a03857",
+    "url": "/dkos/static/js/runtime-main.68d2069b.js"
+  }
+]);
